2dbf8268343b1534fe6ffbc354461ad73dfcaa33
alvarob27 <alvarobarcena27@gmail.com>
2024-03-18 18:22:51 +0100

    Actualizar función POST en signup.ts para validar y almacenar información del usuario

--- src/pages/api/signup.ts
+++ src/pages/api/signup.ts
@@ -1,44 +1,93 @@
-// pages/api/signup.ts
-import { lucia } from "@/services/auth";
+// Importamos las funciones y tipos necesarios de nuestras dependencias
+import { lucia } from "@/auth";
 import { generateId } from "lucia";
 import { Scrypt } from "lucia";
 import { connection as db } from "@/pages/api/items";
 import type { APIContext } from "astro";
+import type { RowDataPacket } from "mysql2/promise";
 
+// Definimos una función asíncrona POST que será utilizada para manejar las solicitudes POST a nuestra API
 export async function POST(context: APIContext): Promise<Response> {
-	const formData = await context.request.formData();
-	const username = formData.get("username");
-	// username must be between 4 ~ 31 characters, and only consists of lowercase letters, 0-9, -, and _
-	// keep in mind some database (e.g. mysql) are case insensitive
-	if (
-		typeof username !== "string" ||
-		username.length < 3 ||
-		username.length > 31 ||
-		!/^[a-z0-9_-]+$/.test(username)
-	) {
-		return new Response("Invalid username", {
-			status: 400
-		});
-	}
-	const password = formData.get("password");
-	if (typeof password !== "string" || password.length < 6 || password.length > 255) {
-		return new Response("Invalid password", {
-			status: 400
-		});
-	}
+  // Obtenemos los datos del formulario de la solicitud
+  const formData = await context.request.formData();
+  const username = formData.get("username");
+  const firstName = formData.get("firstname");
+  const lastName = formData.get("lastname");
+  const email = formData.get("email");
 
-	const userId = generateId(15);
-	const hashedPassword = await new Scrypt().hash(password);
+  // Validamos el nombre de usuario
+  if (
+    typeof username !== "string" ||
+    username.length < 6 ||
+    username.length > 18 ||
+    !/^[a-z0-9_-]+$/.test(username)
+  ) {
+    // Si el nombre de usuario no es válido, devolvemos una respuesta con un estado 400
+    return context.redirect("/signup?error=invalid_username&toast=Error+al+crear+la+cuenta+,+intentalo+de+nuevo");
+  }
 
-	// TODO: check if username is already used
-	await db.query(
-		"INSERT INTO users (id, user_name, user_password) VALUES (?, ?, ?)",
-		[userId, username, hashedPassword]
-	);
+  // Obtenemos y validamos la contraseña
+  const password = formData.get("password");
+  if (
+    typeof password !== "string" || password.length < 8 || password.length > 100
+  ) {
+    // Si la contraseña no es válida, devolvemos una respuesta con un estado 400
+    return context.redirect("/signup?error=invalid_password&toast=Error+al+crear+la+cuenta+,+intentalo+de+nuevo");
+  }
 
-	const session = await lucia.createSession(userId, {});
-	const sessionCookie = lucia.createSessionCookie(session.id);
-	context.cookies.set(sessionCookie.name, sessionCookie.value, sessionCookie.attributes);
+  // Generamos un ID de usuario y una contraseña cifrada
+  const userId = generateId(15);
+  const hashedPassword = await new Scrypt().hash(password);
+  console.log(hashedPassword); // Aquí puedes ver la contraseña cifrada que se almacenará en tu base de datos (¡no la muestres en producción!
 
-	return context.redirect("/");
+  // Comprobamos si ya existe un usuario con el mismo nombre
+  const [existingUser] = await db.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE user_name = ?",
+    [username],
+  );
+  if (existingUser.length > 0) {
+    // Si el usuario ya existe, devolvemos una respuesta con un estado 400
+    return context.redirect("/signup?error=username_exists&toast=Error+al+crear+la+cuenta+,+intentalo+de+nuevo");
+  }
+
+  // validamos el email
+  if (
+    typeof email !== "string" ||
+    email.length < 10 ||
+    email.length > 100 ||
+    !/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(email)
+  ) {
+    // Si el email no es válido, devolvemos una respuesta con un estado 400
+    return context.redirect("/signup?error=invalid_email&toast=Error+al+crear+la+cuenta+,+intentalo+de+nuevo");
+  }
+
+  // comprobamos si ya existe un usuario con el mismo email
+  const [existingEmail] = await db.query<RowDataPacket[]>(
+    "SELECT * FROM users WHERE user_email = ?",
+    [email],
+  );
+
+  if (existingEmail.length > 0) {
+    // Si el email ya existe, devolvemos una respuesta con un estado 400
+    return context.redirect("/signup?error=email_exists&toast=Error+al+crear+la+cuenta+,+intentalo+de+nuevo");
+  }
+
+
+  // Si el usuario no existe, lo insertamos en la base de datos
+  await db.query(
+    "INSERT INTO users (id, user_name, user_password, first_name, last_name, user_email) VALUES (?, ?, ?, ?, ?, ?)",
+    [userId, username, hashedPassword, firstName, lastName, email],
+  );
+
+  // Creamos una nueva sesión para el usuario y establecemos la cookie de sesión
+  const session = await lucia.createSession(userId, {});
+  const sessionCookie = lucia.createSessionCookie(session.id);
+  context.cookies.set(
+    sessionCookie.name,
+    sessionCookie.value,
+    sessionCookie.attributes,
+  );
+
+  // Redirigimos al usuario a la página de inicio
+  return context.redirect("/?account=created");
 }
