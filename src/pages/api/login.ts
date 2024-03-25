ef3a4bf6e3823ac9a757fb52eb38aaead3222e04
alvarob27 <alvarobarcena27@gmail.com>
2024-03-25 21:58:21 +0100

    Actualizar redirecciones de inicio de sesión y mensajes de error

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -14,13 +14,19 @@ export async function POST(context: APIContext): Promise<Response> {
     username.length > 18 ||
     !/^[a-z0-9_-]+$/.test(username)
   ) {
-    return context.redirect("/login?error=invalid_username");
+    return context.redirect(
+      "/login?error=invalid_username&toast=Error+al+iniciar+sesion",
+    );
   }
   const password = formData.get("password");
+  // Regex para validar la contraseña con una longitud mínima de 8 caracteres, al menos una letra mayúscula, una letra minúscula y un dígito
+  const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{8,100}$/;
   if (
-    typeof password !== "string" || password.length < 8 || password.length > 100
+    typeof password !== "string" || !passwordRegex.test(password)
   ) {
-    return context.redirect("/login?error=invalid_password");
+    return context.redirect(
+      "/login?error=invalid_password&toast=Error+al+iniciar+sesion",
+    );
   }
 
   const [rows] = await db.query<RowDataPacket[]>(
@@ -28,12 +34,16 @@ export async function POST(context: APIContext): Promise<Response> {
     [username.toLowerCase()],
   );
   if (rows.length === 0) {
-    return context.redirect("/login?error=incorrect_credentials");
+    return context.redirect(
+      "/login?error=incorrect_credentials&toast=Error+al+iniciar+sesion",
+    );
   }
 
   const existingUser = rows[0];
   if (!existingUser || !existingUser.user_password) {
-    return context.redirect("/login?error=incorrect_credentials");
+    return context.redirect(
+      "/login?error=incorrect_credentials&toast=Error+al+iniciar+sesion",
+    );
   }
 
   const validPassword = await new Scrypt().verify(
@@ -41,7 +51,9 @@ export async function POST(context: APIContext): Promise<Response> {
     password,
   );
   if (validPassword === false) {
-    return context.redirect("/login?error=incorrect_credentials");
+    return context.redirect(
+      "/login?error=incorrect_credentials&toast=Error+al+iniciar+sesion",
+    );
   }
 
   const session = await lucia.createSession(existingUser.id, {});
@@ -52,5 +64,5 @@ export async function POST(context: APIContext): Promise<Response> {
     sessionCookie.attributes,
   );
 
-  return context.redirect("/");
+  return context.redirect("/?account=logged");
 }
