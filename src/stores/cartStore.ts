4329434065e97346973f4a74e39e98f6550dd69f
alvarob27 <alvarobarcena27@gmail.com>
2024-04-25 10:56:56 +0200

    Actualizar lógica de carrito para incluir tamaño y precio de artículo

--- src/stores/cartStore.ts
+++ src/stores/cartStore.ts
@@ -1,4 +1,5 @@
 import { atom, map } from "nanostores";
+import { arrayTallasPorItemId } from "@/services/sizesInfo";
 
 export const isCartOpen = atom(false);
 
@@ -6,24 +7,97 @@ export type CartItem = {
   id: string;
   name: string;
   imageSrc: string;
+  size: string;
+  price: number;
   quantity: number;
 };
 
 export const cartItems = map<Record<string, CartItem>>({});
 
-export function addCartItem({ id, name, imageSrc }) {
-  const existingEntry = cartItems.get()[id];
+export function addCartItem({ id, name, imageSrc, size, price }) {
+  const itemSizeStockQuantity =
+    arrayTallasPorItemId[Number(id) - 1].tallas.find(
+      (talla) => talla.sizeName === size,
+    ).stockQuantity;
+  const key = `${id}-${size}`; // usa una combinación de id y size como clave
+  const existingEntry = cartItems.get()[key];
   if (existingEntry) {
-    cartItems.setKey(id, {
+    if (existingEntry.quantity >= itemSizeStockQuantity) {
+      console.warn(
+        `No se puede añadir más de ${itemSizeStockQuantity} unidades del artículo con id ${id} y size ${size}.`,
+      );
+      return;
+    }
+    cartItems.setKey(key, {
       ...existingEntry,
       quantity: existingEntry.quantity + 1,
     });
   } else {
-    cartItems.setKey(id, {
+    cartItems.setKey(key, {
       id,
       name,
       imageSrc,
+      size,
+      price,
       quantity: 1,
     });
   }
+}
+export function removeCartItem(id: string, size: string) {
+  const key = `${id}-${size}`; // usa una combinación de id y size como clave
+  const existingCartItems = cartItems.get();
+  if (existingCartItems[key]) {
+    const updatedCartItems = { ...existingCartItems };
+    delete updatedCartItems[key];
+    cartItems.set(updatedCartItems);
+  } else {
+    console.warn(
+      `No se encontró el artículo con id ${id} y size ${size} en el carrito.`,
+    );
+  }
+}
+
+export function addItemQuantity(id: string, size: string) {
+  const itemSizeStockQuantity =
+    arrayTallasPorItemId[Number(id) - 1].tallas.find(
+      (talla) => talla.sizeName === size,
+    ).stockQuantity;
+  const key = `${id}-${size}`; // usa una combinación de id y size como clave
+  const existingEntry = cartItems.get()[key];
+  if (existingEntry && existingEntry.quantity < itemSizeStockQuantity) {
+    cartItems.setKey(key, {
+      ...existingEntry,
+      quantity: existingEntry.quantity + 1,
+    });
+  } else {
+    console.warn(
+      `No se encontró el artículo con id ${id} y size ${size} en el carrito o la cantidad máxima ya se ha alcanzado.`,
+    );
+  }
+}
+
+export function subtractItemQuantity(id: string, size: string) {
+  const key = `${id}-${size}`; // usa una combinación de id y size como clave
+  const existingEntry = cartItems.get()[key];
+  if (existingEntry) {
+    if (existingEntry.quantity === 1) {
+      removeCartItem(id, size);
+    } else {
+      cartItems.setKey(key, {
+        ...existingEntry,
+        quantity: existingEntry.quantity - 1,
+      });
+    }
+  } else {
+    console.warn(
+      `No se encontró el artículo con id ${id} y size ${size} en el carrito.`,
+    );
+  }
+}
+
+export function totalPrice() {
+  return Object.values(cartItems.get()).reduce(
+    (total, { price, quantity }) => total + price * quantity,
+    0,
+  );
 }
