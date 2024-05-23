a096b6ab28c715855f4db6ec3b7f19b4030bebeb
alvarob27 <alvarobarcena27@gmail.com>
2024-05-23 23:18:59 +0200

    feat: Crear API para obtener información de usuario por ID

---
+++ src/pages/api/get-user-info/[id].ts
@@ -0,0 +1,38 @@
+// Importamos el tipo APIRoute de Astro que nos permite realizar peticiones a la API
+import { type APIRoute } from "astro";
+// Importamos la conexión a la base de datos que hemos creado en el archivo items.ts
+import { connection as db } from "@/pages/api/items";
+
+// Creamos la función GET que se encargará de obtener los datos de la base de datos y devolverlos
+export const GET: APIRoute = async ({ params, request }) => {
+  try {
+    const id = params.id;
+
+    const [rows, fields] = await db.execute(
+      `SELECT * 
+      FROM users  
+      WHERE id = ?;`,
+      [id],
+    );
+
+    return new Response(
+      JSON.stringify({ rows }),
+      {
+        status: 200,
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+    );
+  } catch (error) {
+    return new Response(
+      JSON.stringify({ message: "Error al obtener los datos" }),
+      {
+        status: 500,
+        headers: {
+          "content-type": "application/json",
+        },
+      },
+    );
+  }
+};
