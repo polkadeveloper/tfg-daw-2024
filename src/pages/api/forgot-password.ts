f0f356c8f151957a062940c5b8a323ffe7d7fab3
alvarob27 <alvarobarcena27@gmail.com>
2024-05-23 23:18:38 +0200

    feat: Add forgot password functionality with email notification

--- src/pages/api/forgot-password.ts
+++ src/pages/api/forgot-password.ts
@@ -10,6 +10,8 @@ import { Resend } from "resend";
 import "dotenv/config";
 // Importamos RowDataPacket de mysql2/promise
 import { type RowDataPacket } from "mysql2/promise";
+// Importamos el Email template que hemos creado en React
+import { EmblemForgotPassword } from "@/components/React/emails/ForgotPasswordTemplate";
 // Creamos una instancia de resend
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -17,28 +19,51 @@ export async function POST(context: APIContext): Promise<Response> {
   const formData = await context.request.formData();
   const email = formData.get("email");
 
+  // Comprobamos si el email existe en la base de datos
   const [rows] = await db.query<RowDataPacket[]>(
     "SELECT id FROM users WHERE user_email = ?",
     [email],
   );
 
+  // Obtenemos el id del usuario
   const id = rows[0]?.id;
 
+  // Si el email no existe en la base de datos, devolvemos un error
   if (rows.length === 0) {
-    return context.redirect(
-      "/forgot-password?error=email_not_exists&toast=Error+al+enviar+el+correo+de+restablecimiento",
+    return new Response(
+      JSON.stringify({
+        error: "email_not_found",
+        message: "El correo electrónico no está registrado",
+      }),
+      { status: 400, headers: { "Content-Type": "application/json" } },
     );
   }
 
+  // Obtenemos el nombre de usuario del usuario
+  const [username] = await db.query<RowDataPacket[]>(
+    "SELECT user_name FROM users WHERE id = ?",
+    [id],
+  );
+
+  // Enviamos un correo electrónico al usuario con un enlace para restablecer la contraseña
   await resend.emails.send({
     from: "Polkadev <alvarobarcena@polkadev.es>",
     to: ["alvarobarcena27@gmail.com"],
-    subject: "Hello World",
-    html:
-      `<strong>It works! ${email}</strong><a href="http://localhost:4321/reset-password/${id}">Reset your password</a>`,
+    subject: "Restablece tu contraseña de Emblem",
+    react: EmblemForgotPassword({
+      id: id,
+      username: username[0]?.user_name || "",
+      updatedDate: new Date(),
+    }),
   });
 
-  return context.redirect(
-    `/email-sent/${id}?email=sent`,
+  // Devolvemos un mensaje de éxito
+  return new Response(
+    JSON.stringify({
+      success: "email_sent",
+      message: "El correo electrónico ha sido enviado correctamente",
+      userId: id, // Devolvemos el id del usuario
+    }),
+    { status: 200, headers: { "Content-Type": "application/json" } },
   );
 }
