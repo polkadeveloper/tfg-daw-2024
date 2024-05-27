47ff15407ec4cc59be45bbe309d1398011981749
alvarob27 <alvarobarcena27@gmail.com>
2024-05-27 13:39:36 +0200

    chore: Add @astrojs/db integration to astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import vercel from '@astrojs/vercel/serverless';
 import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
+
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
+  integrations: [tailwind(), react(), db()],
   base: "/",
   output: "server",
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
-  }),
+      enabled: true
+    }
+  })
 });
