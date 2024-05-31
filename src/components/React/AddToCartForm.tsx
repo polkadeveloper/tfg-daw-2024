3c17a1808203472dfa6d0ab65e6f312671617ae6
alvarob27 <alvarobarcena27@gmail.com>
2024-05-31 22:13:45 +0200

    feat: Add user authentication modal for adding items to cart

--- src/components/React/AddToCartForm.tsx
+++ src/components/React/AddToCartForm.tsx
@@ -2,9 +2,15 @@ import React, { useState, useEffect } from "react";
 import { addCartItem, isCartOpen } from "@/stores/cartStore";
 import { arrayTallasPorItemId } from "@/services/sizesInfo";
 
-export default function AddToCartForm({ children, itemInfo }) {
+export default function AddToCartForm({
+  children,
+  user,
+  sessionGoogle,
+  itemInfo,
+}) {
   const [selectedSize, setSelectedSize] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalUserOpen, setIsModalUserOpen] = useState(false);
 
   useEffect(() => {
     if (isModalOpen) {
@@ -14,14 +20,28 @@ export default function AddToCartForm({ children, itemInfo }) {
     }
   }, [isModalOpen]);
 
+  useEffect(() => {
+    if (isModalUserOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isModalUserOpen]);
+
   function addToCart(e) {
     e.preventDefault();
     if (!selectedSize) {
       setIsModalOpen(true);
       return;
     }
+
+    if (!isModalUserOpen && !user && !sessionGoogle) {
+      setIsModalUserOpen(true);
+      return;
+    }
+
     isCartOpen.set(true);
-    
+
     addCartItem({ ...itemInfo, size: selectedSize });
   }
 
@@ -78,7 +98,7 @@ export default function AddToCartForm({ children, itemInfo }) {
       {isModalOpen && (
         <>
           <div className="fixed inset-0 bg-black opacity-50 backdrop-blur-sm z-10"></div>
-          <div className="absolute flex flex-col gap-3 justify-center items-center bg-white text-black font-cabinetGroteskBold p-2 inset-0 m-auto z-10 max-w-[250px] md:max-w-lg h-[300px] rounded-lg">
+          <div className="absolute flex flex-col gap-3 justify-center items-center bg-white text-black font-cabinetGroteskBold p-2 inset-0 m-auto z-10 max-w-[280px] md:max-w-lg h-[330px] rounded-lg">
             <div>
               <svg
                 data-testid="geist-icon"
@@ -96,7 +116,7 @@ export default function AddToCartForm({ children, itemInfo }) {
                 ></path>
               </svg>
             </div>
-            <p className="text-2xl text-pretty ">
+            <p className="text-2xl text-pretty text-center">
               Selecciona una talla antes de añadir al carrito
             </p>
             <p className="text-lg font-cabinetGroteskRegular text-pretty text-center px-2">
@@ -108,6 +128,63 @@ export default function AddToCartForm({ children, itemInfo }) {
               onClick={() => setIsModalOpen(false)}
             >
               Entendido
+            </button>
+          </div>
+        </>
+      )}
+      {isModalUserOpen && (
+        <>
+          <div className="fixed inset-0 bg-black opacity-50 backdrop-blur-sm z-10"></div>
+          <div className="absolute flex flex-col gap-3 justify-center items-center bg-white text-black font-cabinetGroteskBold p-2 inset-0 m-auto z-10 max-w-[280px] md:max-w-lg h-[330px] rounded-lg">
+            <button
+              onClick={() => setIsModalUserOpen(false)}
+              className="absolute top-2 right-2"
+            >
+              <svg
+                data-testid="geist-icon"
+                height="24"
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+                width="24"
+                style={{ color: "currentcolor" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M9.96966 11.0303L10.5 11.5607L11.5607 10.5L11.0303 9.96966L9.06065 7.99999L11.0303 6.03032L11.5607 5.49999L10.5 4.43933L9.96966 4.96966L7.99999 6.93933L6.03032 4.96966L5.49999 4.43933L4.43933 5.49999L4.96966 6.03032L6.93933 7.99999L4.96966 9.96966L4.43933 10.5L5.49999 11.5607L6.03032 11.0303L7.99999 9.06065L9.96966 11.0303Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </button>
+            <div>
+              <svg
+                data-testid="geist-icon"
+                height="64"
+                strokeLinejoin="round"
+                viewBox="0 0 16 16"
+                width="64"
+                style={{ color: "currentcolor" }}
+              >
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M5.75 0C3.95507 0 2.5 1.45507 2.5 3.25V3.75C2.5 5.54493 3.95507 7 5.75 7H6.25C8.04493 7 9.5 5.54493 9.5 3.75V3.25C9.5 1.45507 8.04493 0 6.25 0H5.75ZM4 3.25C4 2.2835 4.7835 1.5 5.75 1.5H6.25C7.2165 1.5 8 2.2835 8 3.25V3.75C8 4.7165 7.2165 5.5 6.25 5.5H5.75C4.7835 5.5 4 4.7165 4 3.75V3.25ZM15.2803 6.28033L15.8107 5.75L14.75 4.68934L14.2197 5.21967L13.375 6.06434L12.5303 5.21967L12 4.68934L10.9393 5.75L11.4697 6.28033L12.3143 7.125L11.4697 7.96967L10.9393 8.5L12 9.56066L12.5303 9.03033L13.375 8.18566L14.2197 9.03033L14.75 9.56066L15.8107 8.5L15.2803 7.96967L14.4357 7.125L15.2803 6.28033ZM1.5 13.1709V14.5H10.5V13.1709C9.68042 11.5377 8.00692 10.5 6.17055 10.5H5.82945C3.99308 10.5 2.31958 11.5377 1.5 13.1709ZM0.0690305 12.6857C1.10604 10.4388 3.35483 9 5.82945 9H6.17055C8.64517 9 10.894 10.4388 11.931 12.6857L12 12.8353V13V15.25V16H11.25H0.75H0V15.25V13V12.8353L0.0690305 12.6857Z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div>
+            <p className="text-2xl text-pretty ">
+              Debes iniciar sesión para añadir al carrito
+            </p>
+            <p className="text-lg font-cabinetGroteskRegular text-pretty text-center px-2">
+              Antes de añadir un producto a tu carrito necesitamos que inicies
+              sesión.
+            </p>
+            <button
+              className="px-4 py-2 text-lg bg-indigo-700 hover:bg-indigo-500 text-white rounded-lg"
+              onClick={() => (window.location.href = "/login")}
+            >
+              Iniciar sesión
             </button>
           </div>
         </>
