bb0fdc749b333fc5445256eeca70b66fbb65c1c6
alvarob27 <alvarobarcena27@gmail.com>
2024-06-05 17:01:26 +0200

    feat: Añadir API para crear sesión de Stripe

---
+++ src/pages/api/create-stripe-session.ts
@@ -0,0 +1,55 @@
+import { type APIContext } from "astro";
+import Stripe from "stripe";
+const STRIPE_PUBLIC_API_KEY = import.meta.env.STRIPE_PUBLIC_API_KEY;
+const stripe = new Stripe(import.meta.env.STRIPE_PRIVATE_API_KEY);
+
+export async function POST(context: APIContext) {
+  // Parsea el cuerpo de la solicitud a JSON
+  const body = await context.request.text();
+  const { cartItems, origin } = JSON.parse(body);
+
+  console.log(cartItems);
+
+  // Transforma los cartItems para que se ajusten al formato de Stripe
+  const line_items = Object.values(cartItems).map((item: any) => ({
+    price_data: {
+      currency: "eur",
+      product_data: {
+        name: `${item.name} - ${item.size}`,
+        images: [item.imageSrc],
+      },
+      unit_amount: parseFloat(item.price) * 100, // Stripe espera el precio en centavos
+    },
+    quantity: item.quantity,
+  }));
+
+  const session = await stripe.checkout.sessions.create({
+    ui_mode: "embedded",
+    mode: "payment",
+    allow_promotion_codes: true,
+    currency: "eur",
+    automatic_tax: { enabled: true },
+    return_url: `http://localhost:4321/return?session_id={CHECKOUT_SESSION_ID}`,
+    redirect_on_completion: "always",
+    line_items,
+    billing_address_collection: "required",
+    payment_method_types: ["card", "paypal"],
+    phone_number_collection: {
+      enabled: true,
+    },
+    after_expiration: {
+      recovery: {
+        enabled: true,
+        allow_promotion_codes: true,
+      },
+    },
+  });
+
+  // Devuelve el ID de la sesión en la respuesta
+  return new Response(JSON.stringify({ id: session.client_secret }), {
+    status: 200,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+}
